3cb96e46612ae8fec4f193dce3b86e652f3b8905
Mclorm3 <125039348+Mclorm3@users.noreply.github.com>
2023-08-14 10:18:23 +0000

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,6 +1,6 @@
  #1) Terraform block
-/* terraform {
-     required_version= "~> 1.0"  1.1.4./5/6/7   1.2/3/4/5  1.1.4/5/6/7
+ terraform {
+     required_version= "~> 1.0"  #1.1.4./5/6/7   1.2/3/4/5  1.1.4/5/6/7
      required_providers {
        aws = {
          source  = "hashicorp/aws"
